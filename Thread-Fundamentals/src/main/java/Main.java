6b182de310dc8315f4b65c20bfdba87dbe497d8d
Jamil9696 <jamilboujada96@web.de>
2022-11-22 21:23:26 +0100

    Adding description

--- Thread-Fundamentals/src/main/java/Main.java
+++ Thread-Fundamentals/src/main/java/Main.java
@@ -14,7 +14,18 @@ public class Main {
     // starting point of the Main Thread
     public static void main(String[] args) {
 
-        // Thread- Lifecycles (NEW -> ,RUNNABLE <->, RUNNING, -> DEAD)
+        // Thread- Lifecycles
+        //     start()         dt = going back from Runnable to Running
+        //  NEW ---> RUNNABLE <--------> RUNNING
+        //             | ^               |
+        //             | |________       | z.B. Thread.sleep(1000 (+ dt))
+        //             v         |       v
+        //            DEAD <---- BLOCKED
+        //                  it's illegal to stop Blocked Threads) !!!
+        //                  interrupt(),
+        //                  stop()(deprecated):
+        //
+        //
         // If I only instantiate the object than the Thread will go to the NEW Stage but will not start yet
         // By calling the start() method the Thead will go into a stage that we call the runnable lifecycle state
         // and from this state, the Java Virtual Machine can take this Thread and execute it at some point
@@ -23,8 +34,11 @@ public class Main {
         // Imagine in some point, the hardware can only execute 2 additional Threads and the application want to execute
         // 4 Threads. It won't be able to execute all 4 of them. In that case the Java Virtual Machine will only place into
         // execution (Running State) as many Threads he can. When a thread finish his Thread it goes into the dead state.
-        // A dead Thread can not be executed twice (for example be calling start() again later on), it will throw a IllegalStateException
+        // A dead Thread can not be executed twice (for example be calling start() again later on),
+        // it will throw a IllegalStateException
         // When we want to execute the same code independently again at some point, we have to instantiate and run a new Thread
+        // Threads can be blocked but after the blocking time is exceeded, the Thread will go back to the runnable state until
+        // he can be executed
 
         EvenNumberThread even = new EvenNumberThread();
 
