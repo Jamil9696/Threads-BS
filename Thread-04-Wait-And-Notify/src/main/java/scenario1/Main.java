9628c5f79ac081bd70da91ff34426b860fc14598
Jamil9696 <jamilboujada96@web.de>
2022-11-30 12:00:05 +0100

    volatile

--- Thread-04-Wait-And-Notify/src/main/java/scenario1/Main.java
+++ Thread-04-Wait-And-Notify/src/main/java/scenario1/Main.java
@@ -13,6 +13,17 @@ public class Main {
 
         // By adding volatile, all changes to the variable c are also
         // transferred to the main memory
+
+        // But it doesn't solve the synchronized problem !!!
+        // (you can see it by running this program. You see that some
+        // numbers will not be printed)
+
+        // !!!! important !!!
+        // Volatile only synchronize the way from each cpu to the memory
+        // but not from the memory back to the cpu
+
+        // to solve it, you use the synchronize-keyword
+
         IncrementingThread i = new IncrementingThread();
         PrintingThread p = new PrintingThread();
         p.start();
